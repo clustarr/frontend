27e0c45a6adef8338bb0fa7737c41eff0314ae81
lucasheld <lucasheld@hotmail.de>
2020-08-09 20:23:46 +0200

    extend ansible api class to run playbooks

--- src/api/AnsibleApi.js
+++ src/api/AnsibleApi.js
@@ -1,6 +1,22 @@
 export default class AnsibleApi {
     static baseUrl = 'http://localhost:5000/api'
 
+    static runPlaybook(body) {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        };
+        return fetch(`${this.baseUrl}/playbook`, requestOptions)
+            .then(response => {
+                return response.json();
+            }).catch(error => {
+                return error;
+            });
+    }
+
     static listInventory() {
         return fetch(`${this.baseUrl}/inventory?inventory=/etc/ansible/proxmox.py`)
             .then(response => {
