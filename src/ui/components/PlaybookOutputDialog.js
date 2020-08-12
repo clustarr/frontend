4c14ca534200b2b14a31c173d5f63663dea963cd
lucasheld <lucasheld@hotmail.de>
2020-08-12 18:40:42 +0200

    create playbook output dialog

---
+++ src/ui/components/PlaybookOutputDialog.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+class PlaybookOutputDialog extends Component {
+    render() {
+        return (
+            <Dialog
+                open={this.props.isOpen}
+                onClose={this.props.handleClose}
+                scroll="paper"
+                fullWidth={true}
+                maxWidth="md"
+            >
+                <DialogTitle id="scroll-dialog-title">Playbook Output</DialogTitle>
+                <DialogContent dividers>
+                    <DialogContentText
+                        id="scroll-dialog-description"
+                        tabIndex={-1}
+                    >
+                        <Typography variant="body1" style={{whiteSpace: 'pre-line'}}>
+                            {this.props.taskOutput}
+                        </Typography>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.props.handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+}
+
+export default PlaybookOutputDialog;
