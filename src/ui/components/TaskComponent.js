525b2a4839bf9512aed91351f859f137f021442c
lucasheld <lucasheld@hotmail.de>
2020-08-13 11:57:28 +0200

    clean up previous

--- src/ui/components/TaskComponent.js
+++ src/ui/components/TaskComponent.js
@@ -2,14 +2,12 @@ import React, {Component} from "react";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
 import PlaybookOutputDialog from "./PlaybookOutputDialog";
-import AnsibleApi from "../../api/AnsibleApi";
 
 class TaskComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dialogOpen: false,
-            taskOutput: ""
+            dialogOpen: false
         };
     }
 
@@ -32,7 +30,6 @@ class TaskComponent extends Component {
                     isOpen={this.state.dialogOpen}
                     handleClose={this.handleDialogClose}
                     task={this.props.task}
-                    taskOutput={this.state.taskOutput}
                 />
                 <ListItem button key={`listitem-${this.props.task.id}`} onClick={this.openDialog}>
                     <ListItemText id={`label-${this.props.task.id}`} primary={this.props.task.state + " | " + this.props.task.id} />
