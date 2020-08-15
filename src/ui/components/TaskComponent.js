7adb2bc6c45fa13575d3f6f6565ae936addcc667
lucasheld <lucasheld@hotmail.de>
2020-08-15 17:20:40 +0200

    use images and progress spinner to display task status

--- src/ui/components/TaskComponent.js
+++ src/ui/components/TaskComponent.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
 import PlaybookOutputDialog from "./PlaybookOutputDialog";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import {Done, ErrorOutline} from "@material-ui/icons";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class TaskComponent extends Component {
     constructor(props) {
@@ -31,10 +34,23 @@ class TaskComponent extends Component {
                     handleClose={this.handleDialogClose}
                     task={this.props.task}
                 />
-                <ListItem button key={`listitem-${this.props.task.id}`} onClick={this.openDialog}>
+                <ListItem
+                    button
+                    key={`listitem-${this.props.task.id}`}
+                    onClick={this.openDialog}
+                >
+                    <ListItemIcon>
+                        {
+                            this.props.task.state === "SUCCESS" ?
+                                <Done /> :
+                                this.props.task.state === "FAILURE" ?
+                                    <ErrorOutline /> :
+                                    <CircularProgress size={24}/>
+                        }
+                    </ListItemIcon>
                     <ListItemText
                         id={`label-${this.props.task.id}`}
-                        primary={`${this.props.task.state}: ${this.props.task.name}`}
+                        primary={this.props.task.name}
                     />
                 </ListItem>
             </React.Fragment>
